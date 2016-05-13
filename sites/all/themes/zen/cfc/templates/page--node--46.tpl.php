584d5bbd20a961c06af3906e87d5a64301a02f33
v.ratyshnyi <ratyshniyv@gmail.com>
2016-05-13 16:02:43 +0300

    all landing sections.

--- sites/all/themes/zen/cfc/templates/page--node--46.tpl.php
+++ sites/all/themes/zen/cfc/templates/page--node--46.tpl.php
@@ -60,19 +60,22 @@
     <!---->
     <!--  </header>-->
 
-  <div id="main">
+    <div id="main" class="main-content">
       <section class="top-story-section">
 
+          <div class="logo">
 
-          <div class="line first"></div>
-          <div class="line second"></div>
-          <div class="line third"></div>
-          <div class="line fourth"></div>
-          <div class="line fifth"></div>
-          <div class="line sixth"></div>
-          <div class="line seventh"></div>
-          <div class="line eighth"></div>
-          <div class="logo"></div>
+
+              <div class="line first"></div>
+              <div class="line second"></div>
+              <div class="line third"></div>
+              <div class="line fourth"></div>
+              <div class="line fifth"></div>
+              <div class="line sixth"></div>
+              <div class="line seventh"></div>
+              <div class="line eighth"></div>
+
+          </div>
 
 
           <ul class="services">
@@ -85,7 +88,6 @@
           </ul>
           <a href="#" class="phone"><span class="phone-number">+380 (44) 492-75-99</span></a>
       </section>
-
       <section class="team-section">
           <div class="member uppercase"><a class="member-info" href="#">
                   <h2 class="title">Тактика</h2>
@@ -189,44 +191,148 @@
 
               </a></div>
 
-          <div class="member uppercase"><a class="member-info" href="#">
-                  <h2 class="title">Тактика</h2>
-                  <span class="line"></span>
-
-                  <h1 class="member-name">Gennadiy</h1>
-
-                  <h1 class="member-name">Kurochka</h1>
-
-                  <h2 class="role">Managing Partner</h2>
-                  <span class="line"></span>
-
-                  <a href="#" class="link mail"><i class="fa fa-envelope-o "></i></a>
-                  <a href="#" class="link linkedin"><i class="fa fa-linkedin-square"></i></a>
-
-
-              </a></div>
-
-
-          <div class="member uppercase"><a class="member-info" href="#">
-                  <h2 class="title">Тактика</h2>
-                  <span class="line"></span>
-
-                  <h1 class="member-name">Gennadiy</h1>
-
-                  <h1 class="member-name">Kurochka</h1>
-
-                  <h2 class="role">Managing Partner</h2>
-                  <span class="line"></span>
-
-                  <a href="#" class="link mail"><i class="fa fa-envelope-o "></i></a>
-                  <a href="#" class="link linkedin"><i class="fa fa-linkedin-square"></i></a>
-
-
-              </a></div>
-
 
       </section>
-      <!---->
+
+        <section class="services-section">
+
+            <ul class="services">
+                <li><a href="#philantropy" data-toggle="modal"> Philantropy & Corporate <br> Social Responsibility
+                        <span class="line"></span></a></li>
+                <li><a href="#goverment" data-toggle="modal">Goverment Relations <br> & Public Affairs
+                        <span class="line"></span></a></li>
+                <li><a href="#investor" data-toggle="modal">Investor Relations <br> & Financial Communications
+                        <span class="line"></span></a></li>
+                <li><a href="#media" data-toggle="modal">Media Relations <br> & Public Affairs
+                        <span class="line"></span>
+                    </a></li>
+                <li><a href="#special" data-toggle="modal">Special Issues <br> & Crises Management
+                        <span class="line"></span></a></li>
+
+                <li><a href="#promotion" data-toggle="modal">Promotion <br> & Event management
+                        <span class="line"></span></a></li>
+            </ul>
+
+
+            <div class="modal fade" role="dialog" id="philantropy">
+                <div class="modal-dialog">
+
+                    <div class="modal-content">
+
+                        <div class="modal-body">
+                            <div class="close">
+
+                                <a href="#" data-dismiss="modal" class="close-button"></a>
+                            </div>
+                            <div class="modal-content-wrapper">
+                                <h1 class="modal-head-text">
+                                    Philantropy & Corporate <br> Social Responsibility
+                                </h1>
+                                <span class="line"></span>
+
+                                <p class="modal-body-text">
+                                    Ми надаємо різноманітні послуги у сфері комунікацій та зв’язків із <br>
+                                    громадськістю органами державної влади. Компанія CFC Consulting має <br>
+                                    великий досвід роботи з національними міжнародними установами.
+                                </p>
+
+                                <p class="modal-body-text">
+                                    Компанія надає свої послуги з налагодження та підтримання діалогу з <br>
+                                    уоядовими організаціями як в Україні так і за кордоном <br>
+
+                                </p>
+                            </div>
+
+                            <div class="modal-animation-wrapper">
+                                <div class="animation">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+
+        <section class="news-section">
+            <div class="news-item">
+                <a class="news-info" href="#">
+                    <h2 class="category">Категорія, Дата</h2>
+
+                    <h1 class="news-title">НОВИНА 1</h1>
+
+                    <p class="preview-text">orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis quam
+                        dignissim erat sodales, vitae fermentum augue faucibus.. </p>
+
+                </a>
+            </div>
+
+            <div class="news-item">
+                <a class="news-info" href="#">
+                    <h2 class="category">Категорія, Дата</h2>
+
+                    <h1 class="news-title">НОВИНА 2</h1>
+
+                    <p class="preview-text">orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis quam
+                        dignissim erat sodales, vitae fermentum augue faucibus.. </p>
+
+                </a>
+            </div>
+
+
+            <div class="news-item">
+                <a class="news-info" href="#">
+                    <h2 class="category">Категорія, Дата</h2>
+
+                    <h1 class="news-title">НОВИНА 3</h1>
+
+                    <p class="preview-text">orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis quam
+                        dignissim erat sodales, vitae fermentum augue faucibus.. </p>
+
+                </a>
+            </div>
+            <a href="#" class="all-news-btn uppercase">Усі новини</a>
+      </section>
+
+        <section class="career-section">
+            <div class="image-wrapper">
+            </div>
+
+            <div class="text-wrapper">
+                <h1 class="title-text uppercase">Ідеологія <br> компанії</h1>
+                <span class="line"></span>
+
+                <p class="about-us-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
+                    eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
+                    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </p>
+
+                <p class="about-us-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
+                    eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
+                    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </p>
+
+
+                <form class="cv-form" action="#">
+                    <input type="text" class='form-input uppercase' placeholder="iм'я">
+                    <input type="text" class='form-input uppercase' placeholder="електронна пошта">
+                    <input type="file" id="cv" class='load-file'>
+                    <label for="cv" class="label uppercase"><span class="icon"></span> <span
+                            class="text">Upload CV</span></label>
+                    <button type="submit" class="submit-btn uppercase">Send cv</button>
+                </form>
+            </div>
+        </section>
+
+        <section class="contacts-section">
+            <div class="map" id="map"></div>
+
+            <div class="contacts-tab">
+            </div>
+        </section>
+
+
+        <!---->
       <!--    <div id="content" class="column" role="main">-->
       <!--      --><?php //print render($page['highlighted']); ?>
       <!--      --><?php //print $breadcrumb; ?>
