bbdaa2a24fe3f98c8fcb24e6f21266268086e59c
vratyshnyi <ratyshniyv@gmail.com>
2017-05-30 02:10:29 +0300

    Added handling dubai map.

--- sites/all/themes/zen/cfc/templates/page--node--45.tpl.php
+++ sites/all/themes/zen/cfc/templates/page--node--45.tpl.php
@@ -641,10 +641,10 @@ if ($lang == 'uk') {
 
             <div class="contacts-tab">
                 <ul class="nav nav-pills">
-                    <li class="active"><a data-toggle="pill" href="#main-office">Kyiv Office</a>
+                    <li id="#contacts_kyiv" class="active"><a data-toggle="pill" href="#main-office" id="#contacts_kyiv">Kyiv Office</a>
                         <span class="line"></span>
                     </li>
-                    <li><a data-toggle="pill" href="#dubai-office">Dubai Office</a>
+                    <li id="#contacts_dubai"><a data-toggle="pill" href="#dubai-office">Dubai Office</a>
                         <span class="line"></span></li>
                 </ul>
 
